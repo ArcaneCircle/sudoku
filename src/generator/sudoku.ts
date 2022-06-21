7e9a4fc2c70a2331824ecedc807f52a95995019d
M4ss1ck <4ndyraul@gmail.com>
2022-06-20 21:49:18 -0400

    clean code

--- src/generator/sudoku.ts
+++ src/generator/sudoku.ts
@@ -1,13 +1,10 @@
 import { getSudoku } from 'sudoku-gen'
 import type { Difficulty } from 'sudoku-gen/dist/types/difficulty.type'
 
-// const [temporaryInitArray, temporarySolvedArray] = getUniqueSudoku(difficulty, e)
-
 const getUniqueSudoku = (difficulty: Difficulty, e?: React.ChangeEvent<HTMLSelectElement>) => {
   let currentDifficulty = difficulty
   if (e)
     currentDifficulty = e.target.value as Difficulty
-  // currentDifficulty = 'easy'
   const sudoku = getSudoku(currentDifficulty)
   const initArray = sudoku.puzzle.replace(/\-/g, '0').split('')
   const solvedArray = sudoku.solution.split('')
